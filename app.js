2667cf7cf3a0109ec4643e7b6abc42dc240beea1
Tia Rose <shortyshort789@aol.com>
2019-06-24 15:13:03 -0700

    updated code to test

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const Q = require('@nmq/q/client');
+const api = new Q('api');
+require('dotenv').config();
+
+const readline = require('readline');
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+
+
+rl.on('line', function(line){
+  Q.publish('api', 'msg', line);
+});
+
+api.subscribe('msg', payload => {
+  console.log(payload);
+});
+
+// while(true){
+//   rl.on('line', function(line){
+//     console.log(line);
+//     api.publish('api', 'sendMessage', line);
+//   });
+
+// }
+
+
+
+
+// let message = process.argv.slice(2).shift();
+
+// // api.publish('api', 'sendMessage', {name: message});
+// // api.subscribe('sendMessage','api', {name: message});
+// api.publish('api', 'sendMessage',{name: message});
+// // api.monitorEvents('togeneral');

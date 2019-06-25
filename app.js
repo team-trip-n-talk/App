933e1ad6da64c5e78762430b4122edd83c1a7195
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-25 08:13:47 -0700

    test changes

---
+++ app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+require('dotenv').config();
+
+const Q = require('@nmq/q/client');
+const api = new Q('api');
+
+
+const readline = require('readline');
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+
+rl.on('line', function(line){
+  let obj = {
+    name: 'jeff',
+    message: line,
+  };
+  console.log(JSON.stringify(obj));
+  Q.publish('api', 'msg', JSON.stringify(obj));
+  // console.log('skdjfalksj');
+  
+});
+
+api.subscribe('msg', (payload) => {
+  // console.log('here in');
+  let msg = JSON.parse(payload);
+  // console.log('kjkljk' + msg);
+  if(msg.name !== 'jeff'){
+    console.log(msg);
+  }
+
+});
+
+// while(true){
+//   rl.on('line', function(line){
+//     console.log(line);
+//     api.publish('api', 'sendMessage', line);
+//   });
+
+// }
+
+
+
+
+// let message = process.argv.slice(2).shift();
+
+// // api.publish('api', 'sendMessage', {name: message});
+// // api.subscribe('sendMessage','api', {name: message});
+// api.publish('api', 'sendMessage',{name: message});
+// // api.monitorEvents('togeneral');

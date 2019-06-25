d30ad77e993d7c0cf81eeede0dba6930da140a28
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-25 08:38:31 -0700

    sockeio progress

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 'use strict';
 require('dotenv').config();
 
-const Q = require('@nmq/q/client');
-const messageQ = new Q('messageQ');
+// const Q = require('@nmq/q/client');
+// const messageQ = new Q('messageQ');
+const io = require('socket.io-client');
+const socket = io.connect(process.env.SERVER);
+
 const utils = require('./utils/messageHandler');
 
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
-messageQ.subscribe('message', (payload) => {
-  utils.displayIncomingMessage(payload);
-});
+socket.on('message', utils.displayIncomingMessage);
+
+// messageQ.subscribe('message', (payload) => {
+//   utils.displayIncomingMessage(payload);
+// });
+
+// rl.on('line', (line) => {
+//   let name = 'Jeff';
+//   Q.publish('messageQ', 'message', utils.prepMessage(name, line));  
+// });
 
 rl.on('line', (line) => {
   let name = 'Jeff';

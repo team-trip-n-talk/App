7faba2306360304dc693c743586de3ecf8f78b0d
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-25 08:18:20 -0700

    latest version

--- app.js
+++ app.js
@@ -1,51 +1,18 @@
 'use strict';
-
 require('dotenv').config();
 
 const Q = require('@nmq/q/client');
-const api = new Q('api');
-
+const messageQ = new Q('messageQ');
+const utils = require('./utils/messageHandler');
 
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
-
-
-rl.on('line', function(line){
-  let obj = {
-    name: 'jeff',
-    message: line,
-  };
-  console.log(JSON.stringify(obj));
-  Q.publish('api', 'msg', JSON.stringify(obj));
-  // console.log('skdjfalksj');
-  
+messageQ.subscribe('message', (payload) => {
+  utils.displayIncomingMessage(payload);
 });
 
-api.subscribe('msg', (payload) => {
-  // console.log('here in');
-  let msg = JSON.parse(payload);
-  // console.log('kjkljk' + msg);
-  if(msg.name !== 'jeff'){
-    console.log(msg);
-  }
-
+rl.on('line', (line) => {
+  let name = 'Jeff';
+  Q.publish('messageQ', 'message', utils.prepMessage(name, line));  
 });
-
-// while(true){
-//   rl.on('line', function(line){
-//     console.log(line);
-//     api.publish('api', 'sendMessage', line);
-//   });
-
-// }
-
-
-
-
-// let message = process.argv.slice(2).shift();
-
-// // api.publish('api', 'sendMessage', {name: message});
-// // api.subscribe('sendMessage','api', {name: message});
-// api.publish('api', 'sendMessage',{name: message});
-// // api.monitorEvents('togeneral');

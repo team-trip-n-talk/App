aef428d6c2bc6fd7106ca6d7176173a3097cd38c
felipedelatorre <felipede_latorre@hotmail.com>
2019-06-25 08:13:30 -0700

    add eslint script

--- __test__/utils.test.js
+++ __test__/utils.test.js
@@ -1,6 +1,13 @@
 'use strict';
 
+const utils = require('../utils/messageHandler');
+
 describe('Utils module', () => {
+  
+
+  describe('displayIncomingMessage()', () => {
+    
+  });
   
   it('should ', () => {
     expect(true).toBeTruthy();

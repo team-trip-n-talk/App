022d83329e47b88a5b1e4e83a031ab3b7bffdbb7
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-27 11:25:04 -0700

    doc work

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const utils = require('./utils/messageHandler');
 const readline = require('readline');
 
 //Readline variable creation
-const rl = readline.createInterface(process.stdin, process.stdout);
+const readlineInterface = readline.createInterface(process.stdin, process.stdout);
 
 // Prepare the express app
 const app = express();
@@ -24,34 +24,19 @@ app.use('/docs', express.static('docs'));
 
 const io = require('socket.io-client');
 
-/**
-* @method io.connect
-* @param {string} SERVER - server environment variable
-* @returns {object} sends out query object containing environment token variable
-* @desc establishes server connection and sends up the users token for authentication on the server
-*/
-
 const socket = io.connect(process.env.SERVER, {
   query: {token: process.env.TOKEN},
 });
 
-/**
-* @method socket.on
-* @param {string} message - channel / chatroom name
-* @param {function} utils.displayIncomingMessage - function to capture and display incoming messages
-* @returns {object} message
-* @desc recieves messages from server and displays them
-*/
-
 socket.on('message', utils.displayIncomingMessage);
 
 /**
-* @method rl.on
+* @method readlineHandler
 * @param {string} line - reads lines from the command line
 * @returns {object} emits recieved input from command line wrapped in an object with username attached
 * @desc reads what is typed on the command line combines it with the users name and emmits it
 */
 
-rl.on('line', (line) => {
+readlineInterface.on('line', (line) => {
   socket.emit('message', utils.prepMessage(line));
 });
